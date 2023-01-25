02f4d68c225aeccb58a7d5ebefb4372f51cfa38c
RashadNovruzov <r4shad.novruz0v@gmail.com>
2023-01-25 15:52:42 +0400

    Начальная подготовка проекта

---
+++ src/test/java/org/rashad/springproject/RestApiService/RestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rashad.springproject.RestApiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
